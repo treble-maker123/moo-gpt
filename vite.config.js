4f5e73d9d546a0d124ed3753bb654344d2b87d09
🔥Ziqiang 😈 Guan🔥 <mr@zguan.me>
2026-05-27 16:12:16 -0400

    prepping for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
+  base: "/moo-gpt/",
   plugins: [react()],
   resolve: {
     alias: {
