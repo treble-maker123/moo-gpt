7c679be20458a1fadcf5eb6326f0054fea9ce47b
🔥Ziqiang 😈 Guan🔥 <mr@zguan.me>
2026-05-27 21:51:11 -0400

    v0.1.0

--- docs/assets/BufferResource-LYn5N4AR.js
+++ docs/assets/BufferResource-LYn5N4AR.js
@@ -1,4 +1,4 @@
-import{an as U,a7 as g,S as c,al as S,c as _,d as m,E as I,ak as p}from"./index-Bb_rR72N.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{an as U,a7 as g,S as c,al as S,c as _,d as m,E as I,ak as p}from"./index-CEU3MclN.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
--- docs/assets/RenderTargetSystem-D8wuZKSw.js
+++ docs/assets/RenderTargetSystem-D8wuZKSw.js
@@ -1,4 +1,4 @@
-import{x as Ve,N as oe,M as m,o as Ne,m as je,v as $e,h as le,i as l,a2 as W,B as w,a9 as qe,b as z,y as T,s as V,k as de,u as Ke,T as f,S as g,C as D,an as ue,q as ee,ad as ce,L as he,J as pe,U as fe,g as P,H as Ye,f as E,D as N,z as y,R as B,aa as Je,P as Xe,X as Qe,a as Ze,I as te,V as re,a1 as p,ak as et,e as O,n as tt,am as rt,r as st,w as nt}from"./index-Bb_rR72N.js";import{F as at}from"./Filter-Io3yGtng.js";var it=`in vec2 vMaskCoord;
+import{x as Ve,N as oe,M as m,o as Ne,m as je,v as $e,h as le,i as l,a2 as W,B as w,a9 as qe,b as z,y as T,s as V,k as de,u as Ke,T as f,S as g,C as D,an as ue,q as ee,ad as ce,L as he,J as pe,U as fe,g as P,H as Ye,f as E,D as N,z as y,R as B,aa as Je,P as Xe,X as Qe,a as Ze,I as te,V as re,a1 as p,ak as et,e as O,n as tt,am as rt,r as st,w as nt}from"./index-CEU3MclN.js";import{F as at}from"./Filter-COzQXCl1.js";var it=`in vec2 vMaskCoord;
 in vec2 vTextureCoord;
 
 uniform sampler2D uTexture;
